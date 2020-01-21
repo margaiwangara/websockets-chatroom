d2476b367dc853a90ee30a842a82f14b7155a2a7
margaiwangara <margaiwangara@gmail.com>
2020-01-21 14:42:02 +0300

    modified user model

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,21 +1,25 @@
-import mongoose from "mongoose";
+import { Schema, Model, Document, model } from "mongoose";
 
-interface IUserModel extends mongoose.Document {
+interface IUserModel extends Document {
   username: string;
   email?: string;
   name?: string;
   password?: string;
 }
 
-const userSchema = new mongoose.Schema({
+const userSchema: Schema = new Schema({
   username: {
     type: String,
     required: [true, "Username field is required"],
     unique: true,
     maxlength: [50, "Username must be less than 50 chars long"]
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
   }
 });
 
-const User = mongoose.model<IUserModel>("User", userSchema);
+const User: Model<IUserModel> = model<IUserModel>("User", userSchema);
 
 module.exports = User;
