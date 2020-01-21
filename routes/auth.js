15781984cf07995b9d5f738e5743bc115f5abdd7
margaiwangara <margaiwangara@gmail.com>
2020-01-21 12:57:46 +0300

    Express Typescript - Initial Commit

--- routes/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/api/auth/google", async function(req, res, next) {
-  res.send("google auth");
-});
-
-module.exports = router;
