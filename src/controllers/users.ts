d57b43ec171dae8cb1735b44d3b26947ffc92e74
margaiwangara <margaiwangara@gmail.com>
2020-01-21 18:21:32 +0300

    created mutations for user for update, create and delete

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -4,7 +4,9 @@ import {
   GraphQLObjectType,
   GraphQLNonNull,
   GraphQLSchema,
-  GraphQLList
+  GraphQLList,
+  GraphQLScalarType,
+  Kind
 } from "graphql";
 
 const UserType: GraphQLObjectType = new GraphQLObjectType({
@@ -12,20 +14,37 @@ const UserType: GraphQLObjectType = new GraphQLObjectType({
   fields: () => ({
     id: { type: GraphQLString },
     username: { type: GraphQLString },
-    createdAt: { type: GraphQLString }
+    createdAt: {
+      type: new GraphQLScalarType({
+        name: "Date",
+        description: "Date when the user was created",
+        parseValue(value) {
+          return new Date(value);
+        },
+        serialize(value) {
+          return value;
+        },
+        parseLiteral(ast) {
+          if (ast.kind == Kind.INT) {
+            return parseInt(ast.kind, 10);
+          }
+          return null;
+        }
+      })
+    }
   })
 });
 
 const RootQuery: GraphQLObjectType = new GraphQLObjectType({
   name: "RootQuery",
   fields: {
-    getUsers: {
+    users: {
       type: new GraphQLList(UserType),
       resolve(parent, args) {
-        return User.find().then(data => data);
+        return User.find().then(result => result);
       }
     },
-    getUser: {
+    user: {
       type: UserType,
       args: {
         userId: { type: new GraphQLNonNull(GraphQLString) }
@@ -37,6 +56,49 @@ const RootQuery: GraphQLObjectType = new GraphQLObjectType({
   }
 });
 
+const MutationQuery: GraphQLObjectType = new GraphQLObjectType({
+  name: "MutationQuery",
+  fields: {
+    createUser: {
+      type: UserType,
+      args: {
+        username: { type: new GraphQLNonNull(GraphQLString) }
+      },
+      resolve(parent, args) {
+        return User.create(args).then(result => result);
+      }
+    },
+    updateUser: {
+      type: UserType,
+      args: {
+        userId: { type: new GraphQLNonNull(GraphQLString) },
+        username: { type: GraphQLString }
+      },
+      resolve(parent, args) {
+        return User.findByIdAndUpdate(
+          args.userId,
+          { username: args.username },
+          {
+            new: true
+          }
+        ).then(result => result);
+      }
+    },
+    deleteUser: {
+      type: UserType,
+      args: {
+        userId: { type: new GraphQLNonNull(GraphQLString) }
+      },
+      resolve(parent, args) {
+        return User.findByIdAndDelete(args.userId).then(() => ({
+          success: true
+        }));
+      }
+    }
+  }
+});
+
 export default new GraphQLSchema({
-  query: RootQuery
+  query: RootQuery,
+  mutation: MutationQuery
 });
