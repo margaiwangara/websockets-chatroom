5894e2136f0a4c1693cd4a3980b2bdac4f0f7b8d
margaiwangara <margaiwangara@gmail.com>
2020-02-04 22:26:03 +0300

    updated mongodb index connect file, removed duplicate dotenv declaration

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,10 @@ import graphqlHTTP from "express-graphql";
 import * as dotenv from "dotenv";
 import * as path from "path";
 import schema from "./graphql/queries";
-import socketio from 'socket.io';
-import * as exphbs from 'express-handlebars';
-import * as http from 'http';
+import socketio from "socket.io";
+import * as exphbs from "express-handlebars";
+import * as http from "http";
+import connectDB from "./models";
 
 // Inits
 const app: Application = express();
@@ -15,60 +16,55 @@ const io = socketio(server);
 // dotenv config
 dotenv.config({ path: path.resolve(__dirname, "../config/config.env") });
 
+// Connect DB
+connectDB();
+
 const hbs: Exphbs = exphbs.create({
-  partialsDir: 'view/partials',
-  layoutsDir: 'views/layouts',
-  extname: '.hbs'
+  partialsDir: "view/partials",
+  layoutsDir: "views/layouts",
+  extname: ".hbs"
 });
 
 // static and templates
-app.engine('hbs', hbs.engine);
+app.engine("hbs", hbs.engine);
 app.set("view engine", "hbs");
-app.set('views', path.resolve(__dirname, '../views'));
+app.set("views", path.resolve(__dirname, "../views"));
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, "../public")));
 app.use(express.urlencoded({ extended: false }));
 
-const msgs = [
-  'Hello', 'How are you?', 'Code', 'Programming'
-];
+const msgs = ["Hello", "How are you?", "Code", "Programming"];
 
 // Socket IO @TODO: Split to different file
-io.on('connection', function(socket){
-  console.log('User connected');
+io.on("connection", function(socket) {
+  console.log("User connected");
 
   // on message send
-  socket.on('chat message', function(msg){
+  socket.on("chat message", function(msg) {
     msgs.push(msg);
-    io.emit('chat message', msg);
-    io.emit('load messages', msgs);
+    io.emit("chat message", msg);
+    io.emit("load messages", msgs);
   });
 
-  socket.on('load messages', function(){
-    io.emit('load messages', msgs);
+  socket.on("load messages", function() {
+    io.emit("load messages", msgs);
   });
   // on disconnect
-  socket.on('disconnect', function(){
-    console.log('User disconnected');
+  socket.on("disconnect", function() {
+    console.log("User disconnected");
   });
-
 });
 
 // Routes
 const graphqlOptions = { graphiql: true, schema };
 app.use("/graphql", graphqlHTTP(graphqlOptions));
-app.get(
-  "/",
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      return res.render("home", { title: 'Home' });
-    } catch (error) {
-      next(error);
-    }
+app.get("/", async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    return res.render("home", { title: "Home" });
+  } catch (error) {
+    next(error);
   }
-);
-
-
+});
 
 const PORT: number = parseInt(`${process.env.PORT}`, 10) || 5000;
 
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,26 +1,27 @@
 import mongoose from "mongoose";
 import User from "./User";
 import Chat from "./Chat";
-import * as dotenv from 'dotenv';
-import * as path from 'path';
 
-// dotenv config
-dotenv.config({ path: path.resolve(__dirname, "../../config/config.env") });
 // Init
-const debug: boolean = process.env.NODE_ENV == "development" ? true : false;
-const uri: string = `${process.env.MONGO_URI}`;
+const mongoConfig: object = {
+  useFindAndModify: false,
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+};
 
-mongoose.set("debug", debug);
-mongoose
-  .connect(uri, {
-    useFindAndModify: false,
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(conn =>
-    console.log(`MongoDB Connected: ${(conn.connection as any).host}`)
-  )
-  .catch(error => console.log(error));
+export default async function() {
+  const debug: boolean = process.env.NODE_ENV == "development" ? true : false;
+  mongoose.set("debug", debug);
+  try {
+    const conn = await mongoose.connect(
+      `${process.env.MONGO_URI}`,
+      mongoConfig
+    );
+    console.log(`MongoDB Connected: ${(conn.connection as any).host}`);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 export { User, Chat };
