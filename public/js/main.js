0d49017b37255333033b91aa50a070c959fa3f41
margaiwangara <margaiwangara@gmail.com>
2020-02-05 13:06:02 +0300

    removed bootstrap styling link

--- public/js/main.js
+++ public/js/main.js
@@ -5,37 +5,14 @@ var messageBox = document.querySelector('.top');
 // Socket.io Client
 var socket = io();
 
-function handleMessageSubmit(e){
-  if(e.key === 'Enter'){
+function handleMessageSubmit(e) {
+  if (e.key === 'Enter') {
     e.preventDefault();
-    // submit message here
-    socket.emit('chat message', e.target.value);
     // clear input
     e.target.value = '';
 
     return false;
   }
-  
-  
 }
-
-socket.emit('load messages');
-
-socket.on('load messages', function(msgs){
-  msgs.forEach(function(value){
-    var el = document.createElement('span');
-    el.innerHTML = value;
-    messageBox.appendChild(el);
-  });
-});
-
-// display chat messages
-// socket.on('chat message', function(msg){
-//   var el = document.createElement('span');
-//   el.innerHTML = msg;
-//   messageBox.appendChild(el);
-// });
-
-
 
 messageInput.addEventListener('keydown', handleMessageSubmit);
