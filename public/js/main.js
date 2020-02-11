8028602e0a48674eed3e16a19dd54db5bb0b8d08
margaiwangara <margaiwangara@gmail.com>
2020-02-11 17:37:29 +0300

    modified styling to add error display, added form events to send data to backend on form submit and reset inputs

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,38 @@
 var messageForm = document.getElementById('message-form');
 var messageInput = document.getElementById('message-input');
-var messageBox = document.querySelector('.top');
+var errorBox = document.querySelector('.error');
 
 // Socket.io Client
 var socket = io();
 
-var button = document.querySelector('.myButton');
-
-if (!button) console.log('no button');
-
-button.addEventListener('click', function(e) {
+messageForm.addEventListener('submit', function(e) {
   e.preventDefault();
-  alert('button clicked');
-  // emit socket
-  socket.emit('message', { message: 'This is a test message' });
+
+  // grab elements
+  var message = e.target.elements.message.value;
+
+  if (!message) {
+    // display message in error message
+    errorBox.innerHTML = 'Message input is required';
+    // display error message
+    errorBox.style.display = 'block';
+
+    setTimeout(function() {
+      errorBox.style.display = 'none';
+    }, 2000);
+
+    return false;
+  }
+
+  // else send message
+  socket.emit('message', { message: message });
+  // clear input
+  e.target.reset();
 });
+
+// button.addEventListener('click', function(e) {
+//   e.preventDefault();
+//   alert('button clicked');
+//   // emit socket
+//   socket.emit('message', { message: 'This is a test message' });
+// });
